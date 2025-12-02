45967f91d145ad13ad44f5b11f576b2522bcd562
Kaan Gocer <65464708+gocerk@users.noreply.github.com>
2025-12-02 22:23:56 +0300

    add project

--- services/chart.js
+++ services/chart.js
@@ -37,8 +37,8 @@ class ChartService {
       const browser = await puppeteer.launch({
         headless: true,
         defaultViewport: { 
-          width: options.width || 800, 
-          height: options.height || 600 
+          width: 1440, 
+          height: 900 
         },
         args: ['--no-sandbox', '--disable-setuid-sandbox']
       });
